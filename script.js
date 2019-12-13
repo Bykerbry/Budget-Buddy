e97f0099ad4afc5fbe9f39384b619ce5fc5fa7ab
Otto K <ottokeres@gmail.com>
2019-12-12 19:58:47 -0500

    added pie chart code & more css for analysis

---
+++ script.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+
+/**
+ * Shows piechart of user spending
+ * 
+ * 
+ */
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+// Draw the chart and set the chart values
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+  ['Task', 'Dollars per Week'],
+  ['Entertainment', 50],
+  ['Food', 50],
+  ['Bills', 500],
+  ['Clothes', 10],
+  ['Other', 100]
+]);
+
+  // Optional; add a title and set the width and height of the chart
+  var options = {
+    'title':'Your Spending',
+    'titleTextStyle': { color: '#FEEEDA',
+        fontName: 'Lato',
+        fontSize: 14,
+        bold: true,
+        },
+    'width':500, 
+    'height':300,
+    'backgroundColor': '#2D5D7C',
+    'fontSize': 14,
+    'pieSliceText': 'percentage',
+    'legend': {position: 'right', textStyle: {color: '#FEEEDA', fontSize: 11}},
+    'legend.alignment': 'end',
+    'legend.position':'labeled',
+    'tooltip.text':'both'
+
+    
+  }
+
+  // Display the chart inside the <div> element with id="piechart"
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  chart.draw(data, options);
+  
+} 
+// end piechart code
