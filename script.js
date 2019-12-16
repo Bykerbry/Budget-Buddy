dcf2672d34430d927f476ee965eccc3c3dfe0aac
Otto K <ottokeres@gmail.com>
2019-12-16 18:24:07 -0500

    added js functions for grabbing inputs from expenses, not working

--- script.js
+++ script.js
@@ -58,8 +58,25 @@ document.addEventListener('readystatechange', _ => {
 
 
 
+// initialize an empty array, that will be filled when user adds expense
+let expenses = []
 
+// function to add object to array 
+let addAmount = (category, amount) => {
+  amount=document.querySelectorAll("exp-amount").value; 
+  category=document.querySelectorAll("exp-frequency-selector").value; 
+  expenses.push({category, amount});
+  return;
+}
 
+// function to add all entertainment expenses
+let entertainmentSum = () => {
+  let total=0;
+  for (i=0; i<expenses.length; i++){
+    total += expenses[i].amount; 
+  }
+  return total;
+}
 
 
 /*** Commented out Pie Chart section so console would not through errors */
@@ -69,45 +86,44 @@ document.addEventListener('readystatechange', _ => {
 //  * 
 //  * 
 //  */
-// google.charts.load('current', {'packages':['corechart']});
-// google.charts.setOnLoadCallback(drawChart);
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
 
-// // Draw the chart and set the chart values
-// function drawChart() {
-//   var data = google.visualization.arrayToDataTable([
-//   ['Task', 'Dollars per Week'],
-//   ['Entertainment', 50],
-//   ['Food', 50],
-//   ['Bills', 500],
-//   ['Clothes', 10],
-//   ['Other', 100]
-// ]);
+// Draws the chart and sets the chart values
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+  ['Task', 'Dollars per Week'],
+  ['Entertainment', entertainmentSum()],
+  ['Food', document.querySelectorAll("exp-amount").value],
+  ['Bills', document.querySelectorAll("exp-amount").value],
+  ['Clothes', document.querySelectorAll("exp-amount").value],
+  ['Other', document.querySelectorAll("exp-amount").value],
+  ['Available', ]
+]);
 
-//   // Optional; add a title and set the width and height of the chart
-//   var options = {
-//     'title':'Your Spending',
-//     'titleTextStyle': { color: '#FEEEDA',
-//         fontName: 'Lato',
-//         fontSize: 14,
-//         bold: true,
-//         },
-//     'width':500, 
-//     'height':300,
-//     'backgroundColor': '#2D5D7C',
-//     'fontSize': 14,
-//     'pieSliceText': 'percentage',
-//     'legend': {position: 'right', textStyle: {color: '#FEEEDA', fontSize: 11}},
-//     'legend.alignment': 'end',
-//     'legend.position':'labeled',
-//     'tooltip.text':'both'
+  // Optional; add a title and set the width and height of the chart
+  var options = {
+    'title':'Your Spending',
+    'titleTextStyle': { color: '#FEEEDA',
+        fontName: 'Lato',
+        fontSize: 14,
+        bold: true,
+        },
+    'width':500, 
+    'height':300,
+    'backgroundColor': '#2D5D7C',
+    'fontSize': 14,
+    'pieSliceText': 'percentage',
+    'legend': {position: 'right', textStyle: {color: '#FEEEDA', fontSize: 11}},
+    'legend.alignment': 'end',
+    'legend.position':'labeled',
+    'tooltip.text':'both'
+  }
 
-    
-//   }
-
-//   // Display the chart inside the <div> element with id="piechart"
-//   var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-//   chart.draw(data, options);
+  // Display the chart inside the <div> element with id="piechart"
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  chart.draw(data, options);
   
-// } 
+} 
 // // end piechart code
 
